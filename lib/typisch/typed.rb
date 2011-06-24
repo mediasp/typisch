6908cb17b5e5991d4f990a89642b06df595935c3
Matthew Willson <matthew@mediasp.com>
2011-06-24 15:20:45 +0100

    Include type-checking wrapper methods in Typed

--- lib/typisch/typed.rb
+++ lib/typisch/typed.rb
@@ -8,44 +8,74 @@ module Typisch
   # To be used like so:
   #
   # class Foo
-  #   extend Typisch::Typed
+  #   include Typisch::Typed
   #   register_type do
   #     property :foo, :bar
   #     ...
   #   end
   # end
   module Typed
-    def type
-      @type or raise "Forgot to register_type for Typisch::Typed class"
+    def self.included(klass)
+      super
+      klass.send(:extend, ClassMethods)
     end
 
-    def type_of(property_name)
-      @type[property_name]
-    end
-
-  private
-    def register_type(in_registry = Typisch.global_registry, register_as_symbol = to_s.to_sym, &block)
-      raise "Type already registered for #{self}" if @type
-      klass = self; type = nil
-      in_registry.register do
-        type = object(klass, &block)
-        register(register_as_symbol, type)
-      end
-      @type = type
-      @type.property_names_to_types.map do |name, type|
-        # watch out: type may be a named placeholder at this point, so
-        # don't try poking at it too hard
-        define_typed_attribute(name)
-        alias_method(:"#{name}?", name) if Type::Boolean === type
+    def type_check(full_check=false)
+      if full_check
+        self.class.type === self or raise TypeError, "failed to type-check"
+      else
+        self.class.type.property_names.each {|a| type_check_property(a, false)}
       end
     end
 
-    # override this if you want your own funky attributes instead of a vanilla attr_accessor
-    # for any typed properties.
-    def define_typed_attribute(name)
-      attr_accessor(name)
+    def type_check_property(name, full_check=false)
+      type = self.class.type_of(name) or raise NameError, "no typed property #{name} in #{self.class}"
+      if full_check
+        type === send(name)
+      else
+        type.shallow_check_type(send(name))
+      end or raise TypeError, "property #{name} was expected to be of type #{type}"
+    end
+
+    module ClassMethods
+      def type
+        @type or raise "Forgot to register_type for Typisch::Typed class"
+      end
+
+      def type_of(property_name)
+        @type[property_name]
+      end
+
+    private
+      def register_type(in_registry = Typisch.global_registry, register_as_symbol = to_s.to_sym, &block)
+        raise "Type already registered for #{self}" if @type
+        klass = self; type = nil
+        in_registry.register do
+          type = object(klass, &block)
+          register(register_as_symbol, type)
+        end
+        @type = type
+        @type.property_names_to_types.map do |name, type|
+          # watch out: type may be a named placeholder at this point, so
+          # don't try poking at it too hard
+          define_typed_attribute(name)
+          alias_method(:"#{name}?", name) if Type::Boolean === type
+        end
+      end
+
+      # override this if you want your own funky attributes instead of a vanilla attr_accessor
+      # for any typed properties.
+      def define_typed_attribute(name)
+        attr_accessor(name)
+      end
     end
   end
 
-#  class Typisch::Typed
+  class Typisch::TypedStruct
+    include Typisch::Typed
+
+    def initialize(properties={})
+      properties.each {|p,v| instance_variable_set("@#{p}", v)}
+    end
+  end
 end
--- test/test_typed_struct.rb
+++ test/test_typed_struct.rb
@@ -10,7 +10,7 @@ describe "Typed" do
   it "should help you register a new object type for a class or module, registering it by default in the global registry and by a symbol corresponding to the class/module name" do
     Typisch.register do
       class Foo
-        extend Typed
+        include Typed
         register_type do
           property :title, :string
           property :bar, :Bar
@@ -18,7 +18,7 @@ describe "Typed" do
       end
 
       module Bar
-        extend Typed
+        include Typed
         register_type do
           property :title, :string
           property :foo, :Foo
@@ -39,7 +39,7 @@ describe "Typed" do
 
   it "should let you specify what to register it as and a particular registry in which to do so" do
     class Baz
-      extend Typed
+      include Typed
       SPECIAL_REGISTRY = Typisch::Registry.new
 
       register_type(SPECIAL_REGISTRY, :type_of_baz) do
@@ -50,6 +50,65 @@ describe "Typed" do
     baz_type = Baz::SPECIAL_REGISTRY[:type_of_baz]
     assert_instance_of Type::Object, baz_type
     assert_equal Baz, baz_type.class_or_module
-
   end
+
+  class Abc < TypedStruct
+    register_type do
+      property :prop1, :boolean
+      property :prop2, :sequence, :integer
+    end
+  end
+
+  it "should let you type-check instances of the class as a whole" do
+    assert Abc.new(:prop1 => true, :prop2 => [123]).type_check
+    assert_raises(TypeError) {Abc.new(:prop1 => true, :prop2 => nil).type_check}
+    assert_raises(TypeError) {Abc.new(:prop1 => 234, :prop2 => [123]).type_check}
+    assert_raises(TypeError) {Abc.new(:prop1 => true).type_check}
+  end
+
+  it "should let you type-check individual properties" do
+    assert Abc.new(:prop1 => true, :prop2 => [123]).type_check_property(:prop1)
+    assert Abc.new(:prop1 => true, :prop2 => [123]).type_check_property(:prop2)
+
+    assert_raises(TypeError) {Abc.new(:prop1 => 'hey', :prop2 => nil).type_check_property(:prop2)}
+    assert Abc.new(:prop1 => true, :prop2 => 'oi').type_check_property(:prop1)
+
+    assert_raises(TypeError) {Abc.new(:prop1 => 234, :prop2 => [123]).type_check_property(:prop1)}
+    assert Abc.new(:prop1 => 234, :prop2 => [123]).type_check_property(:prop2)
+  end
+
+  it "should type-check shallowly by default" do
+    assert Abc.new(:prop1 => true, :prop2 => ['only','checks','that','this','is','a','sequence']).type_check_property(:prop2)
+    assert Abc.new(:prop1 => true, :prop2 => ['only','checks','that','this','is','a','sequence']).type_check_property(:prop1)
+    assert Abc.new(:prop1 => true, :prop2 => ['only','checks','that','this','is','a','sequence']).type_check
+  end
+
+  it "should do a full type-check if you pass true for full_check" do
+    assert_raises(TypeError) do
+      assert Abc.new(:prop1 => true, :prop2 => ['checks','the','members','too']).type_check_property(:prop2, true)
+    end
+    assert_raises(TypeError) do
+      assert Abc.new(:prop1 => true, :prop2 => ['checks','the','members','too']).type_check(true)
+    end
+  end
+
+  it "should declare attributes on the class based on the properties of the object type" do
+    abc = Abc.new
+    assert_respond_to abc, :prop1
+    assert_respond_to abc, :prop2
+    abc.prop1 = true
+    abc.prop2 = [1,2,3]
+    assert_equal true, abc.prop1
+    assert_equal [1,2,3], abc.prop2
+  end
+
+  it "should alias boolean attributes with a question_marked? getter" do
+    abc = Abc.new
+    assert_respond_to abc, :prop1?
+    abc.prop1 = true
+    assert_equal true, abc.prop1?
+    abc.prop1 = false
+    assert_equal false, abc.prop1?
+  end
+
 end
